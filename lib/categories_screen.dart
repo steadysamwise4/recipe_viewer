2593b3965e94212bf47bef759369b05d853e6660
steadysamwise4 <davenportsam44@gmail.com>
2023-02-07 17:15:39 -0600

    fonts and styling, dummy category_meals_screen navigation added

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -16,6 +16,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Recipe Viewer'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 200,
             childAspectRatio: 3 / 2,
--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
 import 'package:flutter/material.dart';
+
+import './category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   const CategoryItem(this.title, this.color, {super.key});
@@ -9,22 +8,40 @@ class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return const CategoryMealsScreen();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).colorScheme.primary,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
         ),
-        borderRadius: BorderRadius.circular(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
       ),
-      child: Text(title),
     );
   }
 }
